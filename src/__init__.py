3b1fd91c29a4e8bfd6ce9fb259a08b88ed7b52bc
Vinícius Gomes <vinixviii@gmail.com>
2025-11-04 15:23:58 -0300

    CORRIGINDO PROJETO

--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-# in#te#nc#io#nal#me#nte va#zi#o pa#ra de#fi#nir o pa#cote
--- src/config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).parent.parent
-DATA_PATH = BASE_DIR / "src" / "data" / "sleep.csv"
-MODEL_PATH = BASE_DIR / "artifacts" / "model.joblib"
-SCALER_PATH = BASE_DIR / "artifacts" / "scaler.joblib"
-THRESH = 0.85 #   cutoff para classificar eficiência baixa
